a65247b7f45b3b17b4e1534de18e0a3249827eba
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-17 14:42:47 +0100

    :fire: refactor code to enforce DRY

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+
+import MovieCard from './MovieCard'
 import axios from 'axios';
 
 const Movie = props => {
@@ -31,27 +33,8 @@ const Movie = props => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
   return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
-    </div>
+    <MovieCard movie={movie}/>
   );
 }
 
